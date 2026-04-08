bbaf9e511a607ad99794ef0579aeb5fbe80c19e0
hanabi-like <thinkpadt480eighteen@gmail.com>
2026-04-08 20:46:24 +0800

    add file_resource

---
+++ include/file_resource.h
@@ -0,0 +1,39 @@
+#ifndef FILE_RESOURCE_H
+#define FILE_RESOURCE_H
+
+#include <sys/stat.h>
+
+class FileResource
+{
+public:
+    static const int FILE_PATH_LEN = 200;
+
+    enum Result
+    {
+        OK = 0,
+        BAD_REQUEST,
+        FORBIDDEN,
+        NOT_FOUND,
+        INTERNAL_ERROR
+    };
+
+    FileResource();
+    ~FileResource();
+
+    void reset();
+    Result load(const char* url);
+
+    char* data() const;
+    int size() const;
+    const char* path() const;
+
+private:
+    void unmap();
+
+private:
+    char g_filePath[FILE_PATH_LEN];
+    char* g_fileBuf;
+    struct stat g_fileStat;
+};
+
+#endif
---
+++ src/file_resource.cpp
@@ -0,0 +1,94 @@
+#include "file_resource.h"
+
+#include <cstring>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+namespace
+{
+    const char *HTML_ROOT = "/home/user/general_server/frontend";
+}
+
+FileResource::FileResource()
+    : g_fileBuf(0)
+{
+    std::memset(g_filePath, '\0', FILE_PATH_LEN);
+    std::memset(&g_fileStat, 0, sizeof(g_fileStat));
+}
+
+FileResource::~FileResource()
+{
+    unmap();
+}
+
+void FileResource::reset()
+{
+    unmap();
+    std::memset(g_filePath, '\0', FILE_PATH_LEN);
+    std::memset(&g_fileStat, 0, sizeof(g_fileStat));
+}
+
+FileResource::Result FileResource::load(const char *url)
+{
+    reset();
+
+    std::strcpy(g_filePath, HTML_ROOT);
+    int len = std::strlen(HTML_ROOT);
+    std::strncpy(g_filePath + len, url, FILE_PATH_LEN - len - 1);
+
+    if (stat(g_filePath, &g_fileStat) < 0)
+        return NOT_FOUND;
+
+    if (!(g_fileStat.st_mode & S_IROTH))
+        return FORBIDDEN;
+
+    if (S_ISDIR(g_fileStat.st_mode))
+        return BAD_REQUEST;
+
+    int fd = open(g_filePath, O_RDONLY);
+
+    if (fd < 0)
+        return NOT_FOUND;
+
+    if (g_fileStat.st_size == 0)
+    {
+        close(fd);
+        return OK;
+    }
+
+    g_fileBuf = static_cast<char *>(mmap(0, g_fileStat.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
+    close(fd);
+
+    if (g_fileBuf == MAP_FAILED)
+    {
+        g_fileBuf = 0;
+        return INTERNAL_ERROR;
+    }
+
+    return OK;
+}
+
+char *FileResource::data() const
+{
+    return g_fileBuf;
+}
+
+int FileResource::size() const
+{
+    return g_fileStat.st_size;
+}
+
+const char *FileResource::path() const
+{
+    return g_filePath;
+}
+
+void FileResource::unmap()
+{
+    if (g_fileBuf)
+    {
+        munmap(g_fileBuf, g_fileStat.st_size);
+        g_fileBuf = 0;
+    }
+}
